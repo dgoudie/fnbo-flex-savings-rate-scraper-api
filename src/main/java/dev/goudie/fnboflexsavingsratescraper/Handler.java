7639599554976479065e527eaa992c9e1ed3d255
Daniel Goudie <34222208+dgoudie@users.noreply.github.com>
2023-03-20 06:50:47 -0500

    Update Handler.java

--- src/main/java/dev/goudie/fnboflexsavingsratescraper/Handler.java
+++ src/main/java/dev/goudie/fnboflexsavingsratescraper/Handler.java
@@ -37,7 +37,7 @@ public class Handler {
         getAndStoreRate();
     }
 
-    @Scheduled(cron = "0 0 17 * * MON-FRI")
+    @Scheduled(cron = "0 0 7 * * MON-FRI")
     private void getAndStoreRate() {
         double rate;
         try {
